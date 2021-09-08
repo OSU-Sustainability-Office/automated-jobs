90ff02e2ea68c285f8a5a65ce15f42bd36355a09
MilanDonhowe <milanaugust@gmail.com>
2021-09-08 16:30:07 -0700

    Merge readings

--- TeslaSolarCity/TeslaSolarArrays.js
+++ TeslaSolarCity/TeslaSolarArrays.js
@@ -13,7 +13,8 @@ const axios = require('axios')
 async function Job(){
     // Read Tesla Panels
     console.log("Reading Tesla Solar City interface...")
-    const TeslaPanelsReadings = await TeslaPanels()
+    let TeslaPanelsReadings = await TeslaPanels()
+
     console.log('Formatting Data...')
     // Format Data
     Object.keys(TeslaPanelsReadings).map(meter_id => {
@@ -31,13 +32,32 @@ async function Job(){
             if (reading['voltage']) reading['voltage'] = reading['voltage'].replace(/\r/g, '')
             reading['total_energy'] = readings['Lifetime Cumulative Energy (kWh)']
             reading['energy_change'] = readings['Energy In Interval (kWh)']
-            FormattedReadings.push(reading)
+            if (readings['Type'] === '"Production"') FormattedReadings.push(reading)
         })
-        TeslaPanelsReadings[meter_id] = FormattedReadings
+
+        // Merge Readings from Meters to get solar panel totals
+        const ReducedReadings = new Map()
+        for (let reading of FormattedReadings) {
+            if (ReducedReadings.has(reading['time'])){
+                let accumulated_readings = ReducedReadings.get(reading['time'])
+                accumulated_readings['current'] = Number(accumulated_readings['current']) + Number(reading['current'])
+                accumulated_readings['voltage'] = Number(accumulated_readings['voltage']) + Number(reading['voltage'])
+                accumulated_readings['total_energy'] = Number(accumulated_readings['total_energy']) + Number(reading['total_energy'])
+                accumulated_readings['energy_change'] = Number(accumulated_readings['energy_change']) + Number(reading['energy_change'])
+                ReducedReadings.set(reading['time'], accumulated_readings)
+            } else {
+                ReducedReadings.set(reading['time'], reading)
+            }
+        }
+        // ReducedReadings -> Array Format
+        const FinalReadings = []
+        for (let reading of ReducedReadings.values()) {
+            FinalReadings.push(reading)
+        }
+        TeslaPanelsReadings[meter_id] = FinalReadings
     })
 
     await UploadEnergyDashboard(TeslaPanelsReadings)
-
 }
 
 async function UploadEnergyDashboard(MeterData) {
