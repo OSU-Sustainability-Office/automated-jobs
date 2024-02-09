4ee4b46ebf5f935f7d15db91a16ca7f02ff68d54
Jeff Huang <82061589+solderq35@users.noreply.github.com>
2024-02-09 10:44:43 -0800

    [PR] Negative Points checker / check-acq general revamp to check all points (#40)
    * add building hidden and meter groups
    
    * add format script and check for null meter groups
    
    * update readme and gitignore
    
    * update meter group string formatting
    
    * update blacklist meters
    
    * add meter group id to blacklist
    
    * formatting
    
    * got all points on api requests
    
    * Revert "Merge branch 'refactor-point-negatives' into add-building-hidden-and-metergroups"
    
    This reverts commit 70b8f6b866b232a1469a9806df470dee69f4054e, reversing
    changes made to 47ed9cbb4594cd0eca0d41b869b5b0e14995bcfc.
    
    * add fix from main
    
    * got negative logic working for one meter, mostly reworked nochange
    
    * mostly done minus phase changes and handling final output
    
    * commented out old output
    
    * figure out multiple points later
    
    * iterator working
    
    * work in progress
    
    * proof of concept with missing, nochange, negative
    
    * remove nochange and missing, due to unsure about aws hosting
    
    * poc for negative data
    
    * flags for neg vs all, poc mostly done
    
    * add gitignore
    
    * remove point from output
    
    * fix flag logic
    
    * fix wrong findgroup logic, add flags, qol improve
    
    * I think the boolean logic is right now
    
    * gitignore
    
    * lowering negpoints cutoff and !200 error catch
    
    * some variable name cleanup, rethink findindex
    
    * nochange logic fixes
    
    * fix nochange and add sort
    
    * phase sign checker
    
    * obj reorder
    
    * timestamps
    
    * reorder final
    
    * summary lines
    
    * should have everything I can think of for now
    
    * clean up prettierignore, output logs
    
    * minor adjustment to not200limit logic for easier testing
    
    * handle rawmode
    
    * aws test
    
    * handle ctrl c
    
    * now works on aws
    
    * ctrl c documentation
    
    * minor fixes to move logs to top of table, fix 200 rejects, code comments
    
    * temp exception for pacificpower meterclass,
    restructure output meterGroups data structure to match
    API more, make sure cleanup is called if too many non200,
    log which meter id get non200, move logs to bottom,
    minor variable renames

---
+++ check-acq/format-allBuildings.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const axios = require("axios");
+const apiUrl =
+  "https://api.sustainability.oregonstate.edu/v2/energy/allbuildings";
+
+axios
+  .get(apiUrl)
+  .then((response) => {
+    if (response.status === 200) {
+      const input = response.data;
+
+      console.log("Acquisuite Data Checker\n");
+
+      const jsonContent = JSON.stringify(input, null, 2);
+      fs.writeFile(`allBuildings.json`, jsonContent, "utf8", function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("See formatted output in allBuildings.json");
+      });
+
+      // Do something with the 'requests' array if needed.
+    } else {
+      console.error("Failed to fetch data from the API.");
+    }
+  })
+  .catch((error) => {
+    console.error("An error occurred while fetching data:", error);
+  });
