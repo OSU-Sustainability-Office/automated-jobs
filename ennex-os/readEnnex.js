9468438258c8d5618f87e9169c84eeac769783be
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-03-10 18:57:13 -0700

    update to get yesterday's date everytime.

--- ennex-os/readEnnex.js
+++ ennex-os/readEnnex.js
@@ -453,8 +453,7 @@ async function uploadMeterData(meterData) {
  * Date Format: MM/DD/YYYY (e.g. "10/07/2021")
  */
 async function getLastLoggedDate() {
-  // return getYesterdayInPST(); // TODO: implement a GET request to the API to get the last logged date. For now, just return yesterday's date.
-  return "03/05/2025"
+  return getYesterdayInPST(); // TODO: implement a GET request to the API to get the last logged date. For now, just return yesterday's date.
 }
 
 (async () => {
