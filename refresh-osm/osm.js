a14a01406448f402ee0916e8e4699e7d3dc4ffec
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-04-23 19:31:55 -0700

    Polish scraper

--- refresh-osm/osm.js
+++ refresh-osm/osm.js
@@ -10,63 +10,67 @@ const DASHBOARD_API = process.argv.includes("--local-api")
   : process.env.DASHBOARD_API;
  
 async function fetchAllBuildingData() {
-    const buildingData = await axios({
-    method: "get",
-    url: `${DASHBOARD_API}/allbuildings`,
-    })
-    return buildingData.data;
-}
-
-async function mapWayIdtoBuildingId() {
-    const buildingData = await fetchAllBuildingData();
-    const mapIdToBuildingId = new Map();
-
-    // Create a map of wayId to building
-    buildingData
-        .filter(building => building.mapId) // Exclude buildings with blank wayId
-        .forEach(building => {
-            const wayId = building.mapId;
-            const buildingId = parseInt(building.id);
-            mapIdToBuildingId.set(wayId, buildingId);
+    try {
+        const response = await axios({
+            method: "get",
+            url: `${DASHBOARD_API}/allbuildings`,
         });
-
-    return mapIdToBuildingId;
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching building data:", error);
+        throw error;
+    }
 }
 
-// Get GeoJSON data from OSM using Overpass API
 async function fetchGeoJSONData(Ids) {
-    const base = 'https://maps.mail.ru/osm/tools/overpass/api';
-    const route = `interpreter?data=[out:xml];way(id:${Ids});(._;>;);out;`
-    const osmXML = await axios({
-        method: "get",
-        url: `${base}/${route}`,
-        headers: {
-             Accept: 'text/xml'
-        },
-    });
-
-    // Parse the OSM XML response
-    const xmlDoc = new DOMParser().parseFromString(osmXML.data, 'text/xml');
-    const geojson = osmtogeojson(xmlDoc);
-    return geojson;
+    try {
+        const base = 'https://maps.mail.ru/osm/tools/overpass/api';
+        const route = `interpreter?data=[out:xml];way(id:${Ids});(._;>;);out;`
+        const osmXML = await axios({
+            method: "get",
+            url: `${base}/${route}`,
+            headers: {
+                 Accept: 'text/xml'
+            },
+        });
+        
+        // Parse the OSM XML response
+        const xmlDoc = new DOMParser().parseFromString(osmXML.data, 'text/xml');
+        const geojson = osmtogeojson(xmlDoc);
+        return geojson;
+    } catch (error) {
+        console.error("Error fetching GeoJSON data:", error);
+        throw error;
+    }
 }
 
-// Update GeoJSON data in MySQL
-async function updateGeoJSONData(buildings) {
-    const response = await axios({
-        method: "put",
-        url: `${DASHBOARD_API}/buildinggeojson`,
-        data: {
-            buildings: buildings
+/* This function creates a map of building objects with the following properties:
+    * Key: The OSM way ID of the building
+    * Value: An object containing the following properties:
+        * buildingId: The ID of the building
+        * buildingName: The name of the building
+        * buildingGroup: The group of the building
+*/
+async function createbuildingMap() {
+    const buildingData = await fetchAllBuildingData();
+    const buildingMap = new Map();
+
+    for (const building of buildingData) {
+        if (!building.mapId) continue; // Skip buildings with blank wayId
+        
+        buildingMap.set(building.mapId, {
+            buildingId: parseInt(building.id),
+            buildingName: building.name,
+            buildingGroup: building.group,
+          });
         }
-    });
-    return response;
+
+    return buildingMap;
 }
 
 // Some buildings are Polygons by definition, but are returned as LineStrings
 // by OSM. This function corrects the geometry type if necessary.
-async function correctGeometry(feature) {
-    // Convert LineString to Polygon if it is a closed shape
+function correctGeometry(feature) {
     if (feature.geometry.type === 'LineString') {
         const coords = feature.geometry.coordinates
         if (coords[0][0] === coords[coords.length - 1][0] && coords[0][1] === coords[coords.length - 1][1]) {
@@ -76,39 +80,81 @@ async function correctGeometry(feature) {
     }
 }
 
+/* The energy dashboard uses these properties to identify and setup
+    * buildings on the map. This function sets the properties of each feature
+    * in the GeoJSON data to match the building objects.
+*/
+function setBuildingProperties(feature, building) {
+    feature.properties = {
+        id: building.buildingId,
+        group: building.buildingGroup,
+        name: building.buildingName,
+    };
+}
+
+/*
+    * This function normalizes the GeoJSON data to ensure the data is ready to
+    * be used in the Energy Dashboard.
+*/
+function normalizeData(buildingMap, geojson) { 
+    const normalizedBuildingArray = [];
+
+    for (const feature of geojson.features) {
+        const wayId = String(feature.id.split('/')[1]);
+        const building = buildingMap.get(wayId);
+      
+        if (building) {
+            // Set the properties of the feature
+            setBuildingProperties(feature, building);
+
+            // Set the geometry type to Polygon if it is a closed shape
+            correctGeometry(feature);
+
+            // Add the feature to the normalized building array
+            normalizedBuildingArray.push({
+                buildingId: building.buildingId,
+                buildingGeoJSON: feature
+            });
+        }
+    }
+
+    return normalizedBuildingArray;
+}
+
+// This function updates the GeoJSON data in the database
+// with the new building properties using the Energy Dashboard API.
+async function updateGeoJSONData(buildings) {
+    try {
+        const response = await axios({
+            method: "put",
+            url: `${DASHBOARD_API}/buildinggeojson`,
+            data: {
+                buildings: buildings
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error("Error updating GeoJSON data:", error);
+        throw error;
+    }
+}
+
 async function main() {
-    // Build a map of wayId to buildingId
-    const mapIdToBuildingId = await mapWayIdtoBuildingId();
+    // Build a map of wayId to building properties
+    const buildingMap = await createbuildingMap();
 
     // Get OSM data for all buildings
-    const wayIds = mapIdToBuildingId.join(',');
+    const wayIds = Array.from(buildingMap.keys()).join(',');
     const geojson = await fetchGeoJSONData(wayIds);
 
-    // Correct the geometry type if necessary
-    for (const feature of geojson.features) {
-        await correctGeometry(feature);
-    }
-
-    // Build a map of buildingId to GeoJSON feature
-    const buildingGeoMap = new Map();
-    for (const feature of geojson.features) {
-      const osmId = feature.id.split('/')[1]; // e.g. "way/123456" → "123456"
-      const buildingId = mapIdToBuildingId.get(osmId);
-      if (buildingId) {
-        const buildingIdInt = parseInt(buildingId);
-        buildingGeoMap.set(buildingIdInt, feature);
-      }
-    }
-
-    // Convert the map to an array of objects
-    const buildingGeoArray = Array.from(buildingGeoMap, ([id, geoJSON]) => ({
-        buildingId: id,
-        buildingGeoJSON: geoJSON
-    }));
+    // Preapre the GeoJSON data
+    const normalizedBuildingArray = normalizeData(buildingMap, geojson);
 
     // Update the database with the new GeoJSON data
-    const updateResponse = await updateGeoJSONData(buildingGeoArray);
-    console.log(updateResponse.data);
+    if (!process.argv.includes("--no-upload")) {
+        const response = await updateGeoJSONData(normalizedBuildingArray);
+        console.log(response.data);
+      }
 }
 
 // Execute the main function
